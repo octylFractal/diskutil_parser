510d38a8c676d6f7e9f18c4c59b7344558df35a6
Kenzie Togami <ket1999@gmail.com>
2016-12-26 12:41:58 -0800

    Add distutils setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(
+    name='diskutil_parser',
+    version='1.0.0',
+    description='Parses diskutil output',
+    author='Kenzie Togami',
+    author_email='ket1999@gmail.com',
+    url='htps://kenzierocks.me',
+    packages=['diskutil_parser']
+)
